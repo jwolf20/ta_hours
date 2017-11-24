093bba36d377dcaf8ade2b8d283eafdb22117174
Alicia Vissuet <aliciavissuet@Alicias-MacBook-Air.local>
2017-11-24 17:28:07 -0600

    working towards adding comments to requests

--- requests/views.py
+++ requests/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Request, Comment
 from .forms import RequestForm
@@ -14,7 +15,8 @@ def requests(request):
 	requests = Request.objects.order_by('date_posted')
 	context = {'requests': requests}
 	return render(request, 'requests/requests.html', context)
-	
+
+@login_required
 def new_request(request):
 	#post a new request
 	if request.method != 'POST':
@@ -25,7 +27,9 @@ def new_request(request):
 		#POST data submitted; process data.
 		form = RequestForm(request.POST)
 		if form.is_valid():
-			form.save()
+			new_request = form.save(commit=False)
+			new_request.owner = request.user
+			new_request.save()
 			return HttpResponseRedirect(reverse('requests:requests'))
 			
 	context = {'form': form}
