3ee982e44777ab5cd66910ed7541df7e060a14dc
Alicia Vissuet <aliciavissuet@Alicias-MacBook-Air.local>
2017-11-24 10:24:41 -0600

    Trying this again

---
+++ ta_hours_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/aliciavissuet/Documents/workspace/ta_hours/ta_hours_env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
