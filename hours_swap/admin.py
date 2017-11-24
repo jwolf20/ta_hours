31236e5b7c54c657dae113376d54756d9d02638e
Alicia Vissuet <aliciavissuet@Alicias-MacBook-Air.local>
2017-11-24 09:18:00 -0600

    First commit

---
+++ hours_swap/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from hours_swap.models import Cover_Request, Cover_Reply
+
+admin.site.register(Cover_Request)
+admin.site.register(Cover_Reply)
---
+++ hours_swap/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HoursSwapConfig(AppConfig):
+    name = 'hours_swap'
